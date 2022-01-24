8fdd0db7e88f0c2d2af943a0e3a449ca63309993
Michał Szeląg <01151576@pw.edu.pl>
2022-01-24 18:57:23 +0100

    Normalize the vector

--- GK1_lab4/ModelNS/Model.cs
+++ GK1_lab4/ModelNS/Model.cs
@@ -62,11 +62,13 @@ namespace GK1_lab4.ModelNS
         private Vector<double> normal(Vertex a, Vertex b, Vertex c)
         {
             //https://math.stackexchange.com/questions/305642/how-to-find-surface-normal-of-a-triangle
+
             double[] N = new double[3];
             N[0] = (b.y - a.y) * (c.z - a.z) - (c.y - a.y) * (b.z - a.z);
             N[1] = (b.z - a.z) * (c.x - a.x) - (b.x - a.x) * (c.z - a.z);
             N[2] = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
-            return DenseVector.OfArray(N);
+
+            return DenseVector.OfArray(N).Normalize(1);
         }
     }
 }
